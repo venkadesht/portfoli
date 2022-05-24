2a8b1954c562f0490dc884134b7cb003fe6468b3
VENKADESH T <venkadeshvenkadesh464@gmail.com>
2022-05-25 00:00:57 +0530

    first commit

---
+++ portfolio/src/components/timeline.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Internship at Tech Volt <span>2022 February - March</span></h2>
+                        <p>I have recently completed my internship with Tech Volt software Pvt Ltd at Coimbatore. In that I have learned basics of Web Development and I have familiar with HTML,CSS,Javascript and Bootstrap.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Undergraduation at BIT<span> 2020 present</span></h2>
+                        <p>I am Currently pursuing my under-graduation studies with major in BANNARI AMMAN INSTITUTE OF TECHNILOGY. I have taken courses like DSA,  Computer Networks,Web Development from the past two years and have better understanding of these subjects.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Primary and Higher Education <span>2009-2020</span></h2>
+                        <p>I have completed my higher secondary education  in SGHSS with major subjects as Physics,Chemistry,Computer Science & Maths with 70% . During my time at school, I have developed interest in learning Computer Science and I have learned basic Knowledge in Computer Science and I have developed interest in Coding by solving simple coding problems.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
